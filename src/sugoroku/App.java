c6ebc74ffa3bfee17d08b0f0c9d25a12b23463df
Yasutake Tomoya <ty0108.98@icloud.com>
2024-06-07 14:48:46 +0900

    GameクラスとApp（main）クラスの作成

---
+++ src/sugoroku/App.java
@@ -0,0 +1,11 @@
+package sugoroku;
+
+public class App {
+
+	public static void main(String[] args) {
+		// ゲーム起動
+		new Game().startUp();
+
+	}
+
+}
---
+++ src/sugoroku/Game.java
@@ -0,0 +1,11 @@
+package sugoroku;
+
+public class Game {
+	/**
+	 * ゲーム起動
+	 */
+	public void startUp() {
+		System.out.println("Game startUp start");
+		System.out.println("Game startUp end");
+	}
+}
